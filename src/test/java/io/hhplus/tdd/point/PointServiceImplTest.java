3496f984c831f15c7ad57696bc68acd5cc6b4fa4
yujin <uuununew@gmail.com>
2025-03-25 16:58:08 +0900

    테스트 코드 작성 - 포인트 사용

--- src/test/java/io/hhplus/tdd/point/PointServiceImplTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceImplTest.java
@@ -81,4 +81,57 @@ class PointServiceImplTest {
         );
     }
 
+    @Test
+    @DisplayName("포인트 사용 - 0이하 금액 사용 시 예외")
+    public void usePointFail_AmountLessOrEqualZero(){
+        long userId = 1L;
+        long wrongAmount = 0L;
+
+        //when //then
+        assertThatThrownBy(() -> pointService.usePoint(userId, wrongAmount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("포인트 사용 - 보유 포인트 초과 사용 시 예외")
+    public void usePointFail_Over(){
+        //given
+        long userId = 1L;
+        long existingPoint = 1000L;
+        long useAmount = 1500L;
+
+        UserPoint existing = new UserPoint(userId, existingPoint, System.currentTimeMillis());
+        when(userPointTable.selectById(userId)).thenReturn(existing);
+
+        //when //then
+        assertThatThrownBy(() -> pointService.usePoint(userId, useAmount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("포인트 사용 - 정상동작")
+    public void usePointSuccess(){
+        //given
+        long userId = 1L;
+        long existingPoint = 2000L;
+        long useAmount = 500L;
+
+        UserPoint existing = new UserPoint(userId, existingPoint, System.currentTimeMillis());
+        when(userPointTable.selectById(userId)).thenReturn(existing);
+
+        //기대값
+        UserPoint expected = new UserPoint(userId, 1500L, System.currentTimeMillis());
+        when(userPointTable.insertOrUpdate(userId, 1500L)).thenReturn(expected);
+
+        //when
+        UserPoint result = pointService.usePoint(userId, useAmount);
+
+        //then
+        assertThat(result.point()).isEqualTo(1500L);
+
+        verify(userPointTable).insertOrUpdate(userId, 1500L);
+        verify(pointHistoryTable).insert(
+                eq(userId), eq(useAmount), eq(TransactionType.USE), anyLong());
+    }
+
 }
