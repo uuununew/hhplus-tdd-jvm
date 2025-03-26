d2fd899a68f49d2b514ec59c82773cf13dc5e184
yujin <uuununew@gmail.com>
2025-03-26 17:41:53 +0900

    주석 처리 수정

--- src/main/java/io/hhplus/tdd/Exception/PointErrorCode.java
+++ src/main/java/io/hhplus/tdd/Exception/PointErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum PointErrorCode implements ErrorCode{
-    USER_ID_NOT_EXIST(HttpStatus.BAD_REQUEST, "존재하지 않는 사용자 ID 입니다."),
+    //USER_ID_NOT_EXIST(HttpStatus.BAD_REQUEST, "존재하지 않는 사용자 ID 입니다."),
     CHARGE_AMOUNT_LESS_THAN_ZERO(HttpStatus.BAD_REQUEST, "충전금액은 0보다 작을 수 없습니다."),
     CHARGE_AMOUNT_GREATER_THAN_MAX(HttpStatus.BAD_REQUEST, "충전금액이 최댓값보다 클 수 없습니다."),
     USE_AMOUNT_LESS_THAN_ZERO(HttpStatus.BAD_REQUEST, "사용금액은 0보다 작을 수 없습니다."),
