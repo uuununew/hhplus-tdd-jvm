581ef4e6a380c2fa9becefce58c086cd5168c2fd
yujin <uuununew@gmail.com>
2025-03-27 13:46:19 +0900

    동시성 제어 : synchronized 로직 추가

--- src/main/java/io/hhplus/tdd/point/PointServiceImpl.java
+++ src/main/java/io/hhplus/tdd/point/PointServiceImpl.java
@@ -30,48 +30,57 @@ public class PointServiceImpl implements PointService{
 
     @Override
     public UserPoint charge(long id, long amount) {
+        // 1. 해당 유저 ReentrantLock을 가져옴
         ReentrantLock lock = userLockManager.getLock(id);
-        lock.lock();
-        try{
-            //포인트 충전
-            UserPoint userPoint = pointRepository.selectById(id);
-            UserPoint charged = userPoint.charge(amount);
-            UserPoint saved = pointRepository.insertOrUpdate(charged);
+        // 2. lock 객체를 모니터로 사용하는 synchronized 블록
+        synchronized (lock){
+            // 3. 명시적으로 Lock을 획득
+            lock.lock();
+            try{
+                //포인트 충전
+                UserPoint userPoint = pointRepository.selectById(id);
+                UserPoint charged = userPoint.charge(amount);
+                UserPoint saved = pointRepository.insertOrUpdate(charged);
 
-            //충전 내역 저장
-            pointHistoryRepository.insert(
-                    PointHistory.createChargeHistory(saved.id(), saved.point(), saved.updateMillis())
-            );
-            return saved;
-        }finally {
-            lock.unlock();
+                //충전 내역 저장
+                pointHistoryRepository.insert(
+                        PointHistory.createChargeHistory(saved.id(), saved.point(), saved.updateMillis())
+                );
+                return saved;
+            }finally {
+                //4. lock 해제
+                lock.unlock();
+            }
         }
-
     }
 
     @Override
     public UserPoint use(long id, long amount) {
+        // 1. 해당 유저 ReentrantLock을 가져옴
         ReentrantLock lock = userLockManager.getLock(id);
-        lock.lock();
-        try{
-            UserPoint userPoint = pointRepository.selectById(id);
+        // 2. lock 객체를 모니터로 사용하는 synchronized 블록
+        synchronized (lock){
+            // 3. 명시적으로 Lock을 획득
+            lock.lock();
+            try{
+                UserPoint userPoint = pointRepository.selectById(id);
 
-            if (userPoint == null) {
-                throw new PointException(PointErrorCode.USER_ID_NOT_EXIST);
+                if (userPoint == null) {
+                    throw new PointException(PointErrorCode.USER_ID_NOT_EXIST);
+                }
+                //포인트 사용
+                UserPoint used = userPoint.use(amount);
+                UserPoint updated = pointRepository.insertOrUpdate(used);
+
+                //사용 내역 저장
+                pointHistoryRepository.insert(
+                        PointHistory.createUseHistory(updated.id(), amount, updated.updateMillis())
+                );
+                return updated;
+            }finally {
+                //4. lock 해제
+                lock.unlock();
             }
-            //포인트 사용
-            UserPoint used = userPoint.use(amount);
-            UserPoint updated = pointRepository.insertOrUpdate(used);
-
-            //사용 내역 저장
-            pointHistoryRepository.insert(
-                    PointHistory.createUseHistory(updated.id(), amount, updated.updateMillis())
-            );
-            return updated;
-        }finally {
-            lock.unlock();
         }
-
     }
-
 }
--- src/main/java/io/hhplus/tdd/point/UserLockManager.java
+++ src/main/java/io/hhplus/tdd/point/UserLockManager.java
@@ -7,8 +7,16 @@ import java.util.concurrent.locks.ReentrantLock;
 
 @Component
 public class UserLockManager {
+    // 유저 ID별로 Lock을 보관하는 맵
     private final ConcurrentHashMap<Long, ReentrantLock> lockMap = new ConcurrentHashMap<>();
 
+    /**
+     * 특정 유저 ID에 대한 Lock을 반환합니다.
+     * 해당 유저에 대한 Lock이 존재하지 않으면 새로 생성해서 저장하고 반환합니다.
+     *
+     * @param userId 사용자 ID
+     * @return 해당 사용자 ID에 대한 전용 ReentrantLock
+     */
     public ReentrantLock getLock(long userId) {
         return lockMap.computeIfAbsent(userId, key -> new ReentrantLock());
     }
